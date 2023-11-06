7fd7a83b28706c79169c919c1c15a1d2649b01a1
saficktcs <141411503+saficktcs@users.noreply.github.com>
2023-11-06 15:45:30 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "ateam-tf-states"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
   }
 }
